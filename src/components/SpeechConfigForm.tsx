288280c8fbed72ea621b2bd803572cf1eedb3de9
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2021-11-16 17:45:57 +0100

    Can enter QnAConfig keys in Settingspage

--- src/components/SpeechConfigForm.tsx
+++ src/components/SpeechConfigForm.tsx
@@ -1,44 +1,43 @@
-import { Button, Grid, IconButton, Paper, TextField } from "@mui/material";
+import { Box, Button, Grid, IconButton, Paper, TextField } from "@mui/material";
 import React from "react";
 import MySpeechConfig from "../models/MySpeechConfig";
 import useInput from "../hooks/useInput";
 import { ArrowBack, Save } from "@mui/icons-material";
 import GravityItemsArea from "./common/GravityItemsArea";
+import QnAConfig from "../models/QnAConfig";
 
 interface SpeechConfigFormProps {
   hideConfigureScreen: () => void;
-  config: MySpeechConfig;
-  setConfig: (config: MySpeechConfig) => void;
+  mySpeechConfig: MySpeechConfig;
+  qnaConfig: QnAConfig;
+  setConfigKeys: (mySpeechConfig: MySpeechConfig, qnaConfig: QnAConfig) => void;
 }
 
 const SpeechConfigForm: React.FC<SpeechConfigFormProps> = ({
   hideConfigureScreen,
-  config: connectionStrings,
-  setConfig: setConnectionStrings,
+  mySpeechConfig,
+  qnaConfig,
+  setConfigKeys,
 }) => {
-  const useInputResourceKey = useInput(
-    connectionStrings.resourceKey,
-    () => "",
-    undefined,
-    false
-  );
+  const useInputResourceKey = useInput(mySpeechConfig.resourceKey);
+  const useInputRegion = useInput(mySpeechConfig.region);
 
-  const useInputRegion = useInput(
-    connectionStrings.region,
-    () => "",
-    undefined,
-    false
-  );
+  const useInputKbId = useInput(qnaConfig.knowledgeBaseId);
+  const useInputAuthEndpointKey = useInput(qnaConfig.authEndpointKey);
+  const useInputBotName = useInput(qnaConfig.botName);
 
   const handleSaveKeys = () => {
-    useInputResourceKey.validate();
-    if (useInputResourceKey.isValid && useInputRegion.isValid) {
-      connectionStrings.resourceKey = useInputResourceKey.value;
-      connectionStrings.region = useInputRegion.value;
-      setConnectionStrings(connectionStrings);
-    }
+    mySpeechConfig.resourceKey = useInputResourceKey.value;
+    mySpeechConfig.region = useInputRegion.value;
+
+    qnaConfig.knowledgeBaseId = useInputKbId.value;
+    qnaConfig.authEndpointKey = useInputAuthEndpointKey.value;
+    qnaConfig.botName = useInputBotName.value;
+    setConfigKeys(mySpeechConfig, qnaConfig);
   };
 
+  const sectionBoxStyles = { border: 1, p: 3, borderRadius: "4px" };
+  const textFieldStyles = { marginTop: "20px" };
   return (
     <>
       <GravityItemsArea>
@@ -47,24 +46,49 @@ const SpeechConfigForm: React.FC<SpeechConfigFormProps> = ({
         </IconButton>
       </GravityItemsArea>
       <Grid>
-        <h1>Configure keys for speech service</h1>
-        <TextField
-          fullWidth
-          label="Resource key"
-          value={useInputResourceKey.value}
-          onChange={useInputResourceKey.handleChange}
-          helperText={useInputResourceKey.error}
-          error={useInputResourceKey.error !== ""}
-        />
-        <TextField
-          style={{ marginTop: "20px" }}
-          fullWidth
-          label="Region"
-          value={useInputRegion.value}
-          onChange={useInputRegion.handleChange}
-          helperText={useInputRegion.error}
-          error={useInputRegion.error !== ""}
-        />
+        <h1>Configure keys</h1>
+        <Box sx={{ textAlign: "left" }}>
+          <h3>Speech service</h3>
+          <Box sx={sectionBoxStyles}>
+            <TextField
+              fullWidth
+              label="Resource key"
+              value={useInputResourceKey.value}
+              onChange={useInputResourceKey.handleChange}
+            />
+            <TextField
+              style={textFieldStyles}
+              fullWidth
+              label="Region"
+              value={useInputRegion.value}
+              onChange={useInputRegion.handleChange}
+            />
+          </Box>
+          <h3>QnAMaker</h3>
+          <Box sx={sectionBoxStyles}>
+            <TextField
+              fullWidth
+              label="KB (Knowledge Base) Id"
+              value={useInputKbId.value}
+              onChange={useInputKbId.handleChange}
+            />
+            <TextField
+              style={textFieldStyles}
+              fullWidth
+              label="AuthEndpoint Key"
+              value={useInputAuthEndpointKey.value}
+              onChange={useInputAuthEndpointKey.handleChange}
+            />
+            <TextField
+              style={textFieldStyles}
+              fullWidth
+              label="Bot Name"
+              value={useInputBotName.value}
+              onChange={useInputBotName.handleChange}
+            />
+          </Box>
+        </Box>
+
         <div>
           <Button
             style={{ marginTop: "20px" }}
