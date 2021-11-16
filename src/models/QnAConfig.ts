f788a389626e1170d44725234eade4902bc92dd6
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2021-11-16 17:40:38 +0100

    Create QnAConfig.ts

---
+++ src/models/QnAConfig.ts
@@ -0,0 +1,8 @@
+export const DefaultQnAConfig = {
+    knowledgeBaseId: "748a9cd7-6acb-43aa-abdb-15c0a9118679",
+    authEndpointKey: " 47778641-988a-4ca7-939c-05fbdbfd07a4",
+    botName: "chitchatdemobot"
+}
+
+type QnAConfig = typeof DefaultQnAConfig
+export default QnAConfig
