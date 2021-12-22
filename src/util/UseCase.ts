21251c2a9db8900cd00664aadb8a9d950956ca44
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2021-12-22 11:24:20 +0100

    corrected description of available languages in bot chat

--- src/util/UseCase.ts
+++ src/util/UseCase.ts
@@ -12,9 +12,9 @@ export interface UseCaseModel{
 
 export const UseCaseModels: {[key in UseCase]: UseCaseModel} = {
     [UseCase.FourLangToSwissTranslation]: {title: "Übersetzung ins Schweizerdeutsche",
-     description: "Sag etwas auf Englisch, Französisch, Italienisch oder Spanisch und lass dir die Übersetzung auf Schweizerdeutsch vorlesen.​"},
-    [UseCase.BotChat]: {title: "Mehrsprachiger Chatbot", description: `Frage mich etwas auf Englisch, Französisch, Italienisch oder Spanisch und lass dir meine Antwort auf Schweizerdeutsch vorlesen.​`},
-    [UseCase.RealtimeTranscription]: {title: "Diktiergerät mit Übersetzung​", 
+     description: "Sag etwas auf Englisch, Französisch, Italienisch oder Spanisch und lass dir die Übersetzung auf Schweizerdeutsch vorlesen."},
+    [UseCase.BotChat]: {title: "Mehrsprachiger Chatbot", description: `Frage mich etwas auf Deutsch, Englisch, Französisch oder Italienisch und lass dir meine Antwort auf Schweizerdeutsch vorlesen.`},
+    [UseCase.RealtimeTranscription]: {title: "Diktiergerät mit Übersetzung", 
     description:"Fang einfach an zu reden, auf Schweizer- oder Hochdeutsch, wie du willst..."},
     [UseCase.NewsReader]: {title: "Latest News by Leni", description: "Hier kannst du nach einem Thema suchen und dir die Suchergebnisse vorlesen lassen."}
 }
