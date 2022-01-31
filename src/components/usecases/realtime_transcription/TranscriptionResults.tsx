a8541c848576586fa453854dec1f8c51685f57d4
jonahsebright <42495766+jonahsebright@users.noreply.github.com>
2022-01-31 14:54:44 +0100

    Recognizing text not displayed if same as last recognized text

--- src/components/usecases/realtime_transcription/TranscriptionResults.tsx
+++ src/components/usecases/realtime_transcription/TranscriptionResults.tsx
@@ -68,13 +68,33 @@ const TranscriptionResults: React.FC<TranscriptionResultsProps> = ({
     }
   });
 
+  const recognizingTextIsLastRecognizedText = () => {
+    // After pause the recognized text and recognizingText are almost identical
+    // The recognizing text does not have a "." or a "?" at the end but the recognized text does
+    // and the first letter may not be in the same case
+    // To see if they should be equal remove any non letters or numbers from both texts, lowercase both texts and check if recognized text contains the recognizing text
+    // and see if length difference is smaller or equal to 1
+    const recognizingText = speechToTextContinuous.recognizingText
+      .toLowerCase()
+      .replace(/[^a-z0-9]/gi, "");
+    const lastRecognizedText = joinedResults[
+      joinedResults.length - 1
+    ]?.recognizedText
+      .toLowerCase()
+      .replace(/[^a-z0-9]/gi, "");
+    return (
+      lastRecognizedText &&
+      lastRecognizedText.includes(recognizingText) &&
+      lastRecognizedText.length - recognizingText.length <= 1
+    );
+  };
+
   if (
     speechToTextContinuous.recognizingText.length > 0 &&
     speechToTextContinuous.translatingText.length > 0 &&
-    speechToTextContinuous.recognizingText !==
-      joinedResults[joinedResults.length - 1]?.recognizedText
+    !recognizingTextIsLastRecognizedText()
   ) {
-    //show currently spoke (not yet recognized text) at bottom of
+    //show currently spoken text (not yet recognized text) at bottom of
     joinedResults.push({
       recognizedText: speechToTextContinuous.recognizingText,
       translatedText: speechToTextContinuous.translatingText,
