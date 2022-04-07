a5109351976a6aa10315b42062d20d80b9f75955
Esteban Luchsinger <esteban.luchsinger@isolutions.ch>
2022-04-07 18:23:38 +0200

    Refactor the main layout

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,83 +1,68 @@
-import { Grid, Typography } from "@mui/material";
+import {Box, Grid, Typography} from "@mui/material";
 import React from "react";
-import { useNavigate } from "react-router-dom";
-
-
+import {useNavigate} from "react-router-dom";
 import UseCaseCard from "./UseCaseCard";
 import UseCase from "../util/UseCase";
 
-interface HomeProps {
-}
-
-const Home: React.FC<HomeProps> = () => {
-  let navigate = useNavigate();
-
-  return (
-    <div>
-      <Typography variant="h2">Grüezi!</Typography>
-      <Typography variant="body1">
-        Hey, ich bin Leni. Ich bin ein Demo-Bot, der Schweizerdeutsch versteht
-        und spricht.
-      </Typography>
-      <Grid container direction="row" alignItems="center" justifyContent="center" mb={4}>
-        <Grid item xs={12} md="auto">
-          <Typography variant="body1" textAlign="center">
-            Mit mir kannst du die Möglichkeiten der Microsoft Azure Cognitive
-            Services ausprobieren.
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md="auto">
-          <img
-            className="Microsoft-Logo"
-            src="images/RWCZER.jpg"
-            alt="Microsoft"
-          />
-        </Grid>
-      </Grid>
-      <Grid container spacing={4}>
-        <Grid item xs={12} md={6}>
-          <UseCaseCard
-            image="images/isolution_zrh_innenarchitektur-86.jpg"
-            imageAlt="translate symbol with swiss flag"
-            title="Übersetzung ins Schweizerdeutsche"
-            details="Sprich mit mir Englisch, Französisch, Italienisch oder Spanisch und ich übersetze es ins Schweizerdeutsche.​"
-            onSelected={() => navigate("/translate")}
-            useCase={UseCase.FourLangToSwissTranslation}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <UseCaseCard
-            image="images/isolution_zrh_innenarchitektur-102.jpg"
-            imageAlt="Bot icon with several language symbols"
-            title="Mehrsprachiger Chat"
-            details="Stelle mir Fragen und ich beantworte Sie dir auf Schweizerdeutsch."
-            onSelected={() => navigate("/chat")}
-            useCase={UseCase.BotChat}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <UseCaseCard
-            image="images/isolution_zrh_innenarchitektur-31.jpg"
-            imageAlt="Recycling icon with several language symbols"
-            title="Diktiergerät mit Übersetzung"
-            details="Diktiere auf Schweizerdeutsch und ich übersetze direkt."
-            onSelected={() => navigate("/transcription")}
-            useCase={UseCase.RealtimeTranscription}
-          />
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <UseCaseCard
-            image="images/isolution_zrh_innenarchitektur-100.jpg"
-            imageAlt="News Icon with 3 lines symbolyzing summary"
-            title="News vorlesen lassen​"
-            details="Suche die aktuellsten News und ich lese sie dir bei Bedarf vor.​"
-            onSelected={() => navigate("/newsreader")}
-            useCase={UseCase.NewsReader}
-          />
-        </Grid>
-      </Grid>
-    </div>
-  );
+const Home: React.FC = () => {
+    let navigate = useNavigate();
+    return (
+        <>
+            <Box display="flex" flexDirection="column" alignItems="center" marginBottom="2rem">
+                <Typography variant="h2">Grüezi</Typography>
+                <Typography variant="h6" component="h1">
+                    Ich bin Leni! Eine neurale Stimme, die Schweizerdeutsch versteht
+                    und spricht.
+                </Typography>
+                <Typography variant="body1">
+                    Mit mir kannst du die Möglichkeiten der Microsoft Azure Cognitive
+                    Services ausprobieren.
+                </Typography>
+            </Box>
+            <Grid container spacing={4}>
+                <Grid item xs={12} md={6}>
+                    <UseCaseCard
+                        image="images/isolution_zrh_innenarchitektur-86.jpg"
+                        imageAlt="translate symbol with swiss flag"
+                        title="Übersetzung ins Schweizerdeutsche"
+                        details="Sprich mit mir Englisch, Französisch, Italienisch oder Spanisch und ich übersetze es ins Schweizerdeutsche.​"
+                        onSelected={() => navigate("/translate")}
+                        useCase={UseCase.FourLangToSwissTranslation}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <UseCaseCard
+                        image="images/isolution_zrh_innenarchitektur-102.jpg"
+                        imageAlt="Bot icon with several language symbols"
+                        title="Mehrsprachiger Chat"
+                        details="Stelle mir Fragen und ich beantworte Sie dir auf Schweizerdeutsch."
+                        onSelected={() => navigate("/chat")}
+                        useCase={UseCase.BotChat}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <UseCaseCard
+                        image="images/isolution_zrh_innenarchitektur-31.jpg"
+                        imageAlt="Recycling icon with several language symbols"
+                        title="Diktiergerät mit Übersetzung"
+                        details="Diktiere auf Schweizerdeutsch und ich übersetze direkt."
+                        onSelected={() => navigate("/transcription")}
+                        useCase={UseCase.RealtimeTranscription}
+                    />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <UseCaseCard
+                        image="images/isolution_zrh_innenarchitektur-100.jpg"
+                        imageAlt="News Icon with 3 lines symbolyzing summary"
+                        title="News vorlesen lassen​"
+                        details="Suche die aktuellsten News und ich lese sie dir bei Bedarf vor.​"
+                        onSelected={() => navigate("/newsreader")}
+                        useCase={UseCase.NewsReader}
+                    />
+                </Grid>
+            </Grid>
+        </>
+    );
 };
 
 export default Home;
